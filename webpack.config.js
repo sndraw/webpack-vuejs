4339ffe8970f47b290dcbe77b04ca09b0b1d7e14
sn <sndraw@163.com>
2018-05-13 13:49:39 +0800

    测试2

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CleanWebpackPlugin = require('clean-webpack-plugin'); //清理文件夹
 var nodeModulesPath = path.join(__dirname, 'node_modules');
 var { VueLoaderPlugin } = require('vue-loader');
-const vueLoaderConfig = require('./app/js/vue-loader.conf')
-
+const vueLoaderConfig = require('./app/js/vue-loader.conf');
 //webpack配置-根据开发模式可改变配置
 var webpackConfig = {
     nameHash: ".[hash:8]",//hash规则
@@ -17,35 +16,51 @@ var webpackConfig = {
         publicPath: "/", //静态文件目录，如果网站路径直接指到dist目录，请注意改为/
         filename: 'js/[name].[hash].js', // 输出文件名
         chunkFilename: 'js/[name].[hash].js', // 按需加载模块输出文件名
-    },
-    plugins: {
-        define: new webpack.DefinePlugin({
-            "process.env": {//上线后的devtool要配置为source-map,有时候为了性能考虑，一定要配置这个插件
-                NODE_ENV: JSON.stringify("production")
-            }
-        }),
-        clean: new CleanWebpackPlugin(['*'], {
-            root: path.resolve(__dirname, './dist'),
-            verbose: true,
-            dry: false,
-            exclude: []
-        }),
-        uglify: new webpack.optimize.UglifyJsPlugin({
-            mangle: {
-                except: ['$super', '$', 'exports', 'require']
-                //以上变量‘$super’, ‘$’, ‘exports’ or ‘require’，不会被混淆
-            },
-            compress: {
-                //supresses warnings, usually from module minification
-                warnings: false
-            }
-        })
     }
 };
 //github打包模式
 if (process.env.NODE_ENV === 'github') {
     process.env.NODE_ENV = 'production';
     webpackConfig.output.publicPath = "/webpack-vuejs/dist/"; // 设置输出目录
+}
+
+var cleanFun=function () {
+    return new CleanWebpackPlugin(['*'], {//清理文件
+        root: path.resolve(__dirname, './dist'),
+        verbose: true,
+        dry: false,
+        exclude: []
+    })
+}
+var CleanFile=function () {
+    if(process.env.NODE_ENV === 'development'){
+        return function () {
+            console.log("开发模式下，无需清理文件");
+        }
+    }
+    return new CleanWebpackPlugin(['*'], {//清理文件
+        root: path.resolve(__dirname, './dist'),
+        verbose: true,
+        dry: false,
+        exclude: []
+    })
+}
+var UglifyJs= function(){
+    if(process.env.NODE_ENV === 'development'){
+        return function () {
+            console.log("开发模式下，无需压缩文件");
+        }
+    }
+    return new webpack.optimize.UglifyJsPlugin({//压缩打包的文件
+        mangle: {
+            except: ['$super', '$', 'exports', 'require']
+            //以上变量‘$super’, ‘$’, ‘exports’ or ‘require’，不会被混淆
+        },
+        compress: {
+            //supresses warnings, usually from module minification
+            warnings: false
+        }
+    })
 }
 //开发者模式
 if (process.env.NODE_ENV === 'development') {
@@ -57,17 +72,6 @@ if (process.env.NODE_ENV === 'development') {
         filename: 'js/[name].js', // 输出文件名
         chunkFilename: 'js/[name].js', // 按需加载模块输出文件名
     };
-    webpackConfig.plugins.define = new webpack.DefinePlugin({
-        "process.env": {//上线后的devtool要配置为source-map,有时候为了性能考虑，一定要配置这个插件
-            NODE_ENV: JSON.stringify("development")
-        }
-    });
-    webpackConfig.plugins.clean = function () {
-        console.log('开发模式下，无需清理文件夹');
-    };//清理文件插件
-    webpackConfig.plugins.uglify = function () {
-        console.log('开发模式下，无需压缩文件');
-    };//压缩文件插件
 }
 module.exports = {
     //页面入口文件配置
@@ -85,51 +89,71 @@ module.exports = {
             vue: 'vue/dist/vue.esm.js',
         },
         //设置require或import的时候可以不需要带后缀
-        extensions: ['.js', '.less', '.css', '.scss','.vue']
+        extensions: ['.js', '.less', '.css', '.scss', '.vue']
     },
     module: {
         //加载器配置
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        css: ExtractTextPlugin.extract({
-                            use: ['css-loader'],
-                            fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
-                        }),
-                        sass: ExtractTextPlugin.extract({
-                            use: ['css-loader', 'sass-loader'],
-                            fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
-                        }),
-                        scss: ExtractTextPlugin.extract({
-                            use: ['css-loader', 'sass-loader'],
-                            fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
-                        }),
-                        js: 'babel-loader!eslint-loader'
-                    },
-                    indentedSyntax: true
-                }
+                loader: 'vue-loader'
+                // options: {
+                //     loaders: {
+                //         css: ExtractTextPlugin.extract({
+                //             use: ['css-loader'],
+                //             fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                //         }),
+                //         sass: ExtractTextPlugin.extract({
+                //             use: ['css-loader', 'sass-loader'],
+                //             fallback: 'vue-style-loader', // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                //             indentedSyntax: true
+                //         }),
+                //         scss: ExtractTextPlugin.extract({
+                //             use: ['css-loader', 'sass-loader'],
+                //             fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                //         }),
+                //         js: 'babel-loader!eslint-loader'
+                //     }
+                // }
             },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
+                loader: 'babel-loader'
             },
-            // {
-            //     test: /\.css$/,
-            //     use: ExtractTextPlugin.extract({
-            //         fallback: 'style-loader',
-            //         use: ['css-loader']
-            //     })
-            // }, {
-            //     test: /\.scss$/,
-            //     use: ExtractTextPlugin.extract({
-            //         fallback: 'style-loader',
-            //         //resolve-url-loader may be chained before sass-loader if necessary
-            //         use: ['css-loader', 'sass-loader']
-            //     })
-            // },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    use: ['css-loader'],
+                    fallback: 'vue-style-loader'
+                })
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'vue-style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                indentedSyntax: true
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'vue-style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                })
+            },
             {
                 test: /\.(png|jpg|gif)$/,
                 loader: 'url-loader',
@@ -139,7 +163,7 @@ module.exports = {
                 }
             }, {
                 test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
-                loader: 'file-loader',
+                loader: 'url-loader',
                 options: {
                     name: 'fonts/[name]' + webpackConfig.nameHash + '.[ext]' //输出目录以及名称
                 }
@@ -156,26 +180,38 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({//全局变量
+            "process.env": process.env.NODE_ENV
+        }),
         new VueLoaderPlugin(),
 
-        webpackConfig.plugins.define,//全局变量
+        CleanFile(),
+
         // new webpack.ProvidePlugin({
         //     Config:'config'
         // }),
-        webpackConfig.plugins.clean,//清理文件
+
+
         // 分离css
-        new ExtractTextPlugin({
-            filename:  (getPath) => {
-                return getPath('css/[name]' + webpackConfig.nameHash + '.css').replace('css/js', 'css');
-            },
+        // new ExtractTextPlugin({
+        //     filename: (getPath) => {
+        //         return getPath('css/[name]' + webpackConfig.nameHash + '.css').replace('css/js', 'css');
+        //     },
+        //     allChunks: true
+        // }),
+
+        new ExtractTextPlugin('css/[name]' + webpackConfig.nameHash + '.css', {
             allChunks: true
         }),
+
+
         // new webpack.optimize.CommonsChunkPlugin({
         //         name: "vendor",
         //         filename: 'js/vendor' + webpackConfig.nameHash + '.js',
         //         async: false
         // }),
-        webpackConfig.plugins.uglify, //压缩打包的文件
+        UglifyJs(),
+
         //允许错误不打断程序
         new webpack.NoErrorsPlugin(),
         new HtmlWebpackPlugin({
@@ -196,7 +232,8 @@ module.exports = {
         historyApiFallback: true,
         progress: true,
         port: 8080,
-        headers: {},
+        headers: {}
+        ,
         proxy: {//接口转发
             '/api': {
                 target: 'http://localhost:8081', //转发地址
@@ -205,7 +242,9 @@ module.exports = {
                     '^/api/(.*)\.json$': '/page/$1'
                 }//路由重写，与target组装成新的地址,如“/api/getlogo”转发到“http://localhost/getlogo”
             }
-        },
+        }
+        ,
         contentBase: process.env.NODE_ENV == 'development' ? 'dev' : 'dist'
     }
-};
+}
+;
